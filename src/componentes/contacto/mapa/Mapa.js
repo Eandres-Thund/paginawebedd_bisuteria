1d299c694417a3d2fd061e622f8d4492daf8f20c
Eandres-Thund <79473850+Eandres-Thund@users.noreply.github.com>
2021-04-15 11:46:28 -0500

    probando

---
+++ src/componentes/contacto/mapa/Mapa.js
@@ -0,0 +1,19 @@
+import React from 'react';
+ 
+class Mapa extends React.Component {
+ 
+  render() {
+ 
+    return (
+ 
+        <div className="embed-responsive embed-responsive-4by3">
+            {/*<iframe className="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15864.98716990164!2d-75.59205477405675!3d6.231161158839353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4429b703fb27a1%3A0xdacf4ad5797fcb2c!2sAlpujarra%2C%20Medell%C3%ADn%2C%20Antioquia!5e0!3m2!1ses!2sco!4v1618277108037!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>*/}
+        </div>
+ 
+    )
+    
+  }
+ 
+}
+ 
+export default Mapa;
