e29c76bd2ed8d37e1c5d269d346f0c1e9052eb50
Eandres-Thund <79473850+Eandres-Thund@users.noreply.github.com>
2021-04-23 19:54:12 -0500

    Links imágenes home
    Se convierten las 3 imágenes de home en links que llevaran a la pagina de galería (pendiente la ruta)

--- src/componentes/home/imagenes/Imagenes.js
+++ src/componentes/home/imagenes/Imagenes.js
@@ -16,17 +16,23 @@ class Imagenes extends React.Component{ //Este componente agrega 3 imagenes cent
             <div className= "row">
                 <div className= "col-lg-4"> {/*Esta clase se usa para dividir en 3 el espacio de trabajo, primera columna*/}
                     <h3 className= "fuente">Pendientes</h3>
-                    <img src={process.env.PUBLIC_URL + 'imagenes/aretesMiyuki.jpg'} className= "mx-auto d-block img-fluid rounded" alt="Imagen Izquierda home"/> {/*Se añaden clases para centrar horizontalmente, ancho de 100%, altura:auto y punta redonda*/}
+                    <a href= "#">
+                        <img src= {process.env.PUBLIC_URL + 'imagenes/aretesMiyuki.jpg'} className= "mx-auto d-block img-fluid rounded" alt="Imagen Izquierda home"/> {/*Se añaden clases para centrar horizontalmente, ancho de 100%, altura:auto y punta redonda*/}
+                    </a>
                 </div>
 
                 <div className= "col-lg-4"> {/*Esta clase se usa para dividir en 3 el espacio de trabajo, segunda columna*/}
                     <h3 className= "fuente">Manillas</h3>
-                    <img src={process.env.PUBLIC_URL + 'imagenes/manillaMiyuki.jpg'} className= "mx-auto d-block img-fluid rounded" alt="Imagen centro home"/> {/*Se añaden clases para centrar horizontalmente, ancho de 100%, altura:auto y punta redonda*/}
+                    <a href= "#">
+                        <img src= {process.env.PUBLIC_URL + 'imagenes/manillaMiyuki.jpg'} className= "mx-auto d-block img-fluid rounded" alt="Imagen centro home"/> {/*Se añaden clases para centrar horizontalmente, ancho de 100%, altura:auto y punta redonda*/}
+                    </a>
                 </div>
 
                 <div className= "col-lg-4"> {/*Esta clase se usa para dividir en 3 el espacio de trabajo, tercera columna*/}
                     <h3 className= "fuente">Collares</h3>
-                    <img src={process.env.PUBLIC_URL + 'imagenes/collarMiyuki.jpg'} className= "mx-auto d-block img-fluid rounded" alt="Imagen derecha home"/> {/*Se añaden clases para centrar horizontalmente, ancho de 100%, altura:auto y punta redonda*/}
+                    <a href= "#">
+                        <img src= {process.env.PUBLIC_URL + 'imagenes/collarMiyuki.jpg'} className= "mx-auto d-block img-fluid rounded" alt="Imagen derecha home"/> {/*Se añaden clases para centrar horizontalmente, ancho de 100%, altura:auto y punta redonda*/}
+                    </a>
                 </div>
 
             </div>
